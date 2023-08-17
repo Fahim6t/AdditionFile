1637b45bbe86d8abb6e2c40537d52e93d50dee9b
dell <dell@VASEEM>
2023-08-17 19:20:27 +0530

    First Commit

---
+++ src/com/fahimJava/Addition.java
@@ -0,0 +1,17 @@
+package com.fahimJava;
+import java.util.Scanner;
+public class Addition {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number1: ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter the number2: ");
+		int num2 = sc.nextInt();
+		int result = num1+num2;
+		System.out.println(result);
+		
+
+	}
+
+}
